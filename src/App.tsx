9e74dad7f49a285f0a77d2af858a0157eb69f5d2
OkadaGenya <g.okada.wilf+github@gmail.com>
2019-10-07 01:24:28 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import styled from 'styled-components';
+import "./App.css";
+
+
+const Test = styled.div`
+`
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Test
+      >aaaaa</Test>
+    </div>
+  );
+};
+
+export default App;
