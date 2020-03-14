15571ec358aed5c0a393b51041e82a8fabe07996
OkadaGenya <g.okada.wilf+github@gmail.com>
2020-03-14 17:07:56 +0900

    add reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
 import React, { useState } from "react";
 import styled from 'styled-components';
-import "./App.css";
+import "./global/App.css";
+import "./global/reset.css";
 
 
-const Test = styled.div`
+const Wrap = styled.div`
+  max-width: 800px;
+  margin: auto;
+`
+const Title = styled.h1`
+  font-size: 30px;
+
+`
+const Para = styled.p`
 `
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Test
-      >aaaaa</Test>
+      <Wrap
+      >
+        <Title>
+          DRILL
+
+
+        </Title>
+        <Para>株式会社DRILLはWebアプリケーションにおけるフロントエンドチーム開発の最適化を行い 継続可能なチーム設計をする会社です。
+        </Para>
+
+      </Wrap>
     </div>
   );
 };
