1ec261198474278c45513c018a005afe8acc2de5
OkadaGenya <g.okada.wilf+github@gmail.com>
2021-01-13 02:18:09 +0900

    switch の追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import "./global/reset.css";
 import "./global/App.css";
 
+
 import {Top} from './component/Top'
+import {Switch} from './component/Switch'
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Top />
+      <Switch />
     </div>
   );
 };
